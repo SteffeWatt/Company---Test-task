4cd996a5ea2725b48220f53b3fbc4e33ecc88cf4
Steffen W.G <steffengravdehaug@hotmail.no>
2023-02-21 21:11:26 +0100

    icons and more table page work
    added icons and did some more work on the table page. also added logout button

--- index.js
+++ index.js
@@ -2,12 +2,18 @@
 let loginPage = document.getElementById("LoginMainPage");
 let TablePage = document.getElementById("TableSection");
 
+let logOut = document.getElementById("LogoutButton");
+
 function hideLoginPage(){
     loginPage.style.display = "none";
     TablePage.style.display = "block";
+
+    logOut.style.display = "block";
 }
 
 function ShowLoginPage(){
     loginPage.style.display = "block";
     TablePage.style.display = "none";
+
+    logOut.style.display = "none";
 }
